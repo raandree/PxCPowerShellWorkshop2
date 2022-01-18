1c491c1760fe23435d9b1e78c6aaf2ca72330256
Raimund Andree <r.andree@live.com>
2022-01-18 14:05:09 +0100

    Added car sample

---
+++ Car/Car.ps1
@@ -0,0 +1,9 @@
+﻿Add-Type -Path D:\Car\Car\bin\Debug\Car.dll
+
+$car1 = New-Object My.Car
+$car1.Manufacturer = 'VW'
+$car1.Color = 'red'
+$car1.Model = 'Golf'
+$car1.Accelerate(50)
+$car1.Accelerate(30)
+$car1
